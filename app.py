353ca9a4c5458ee15852f4143b0bb61327960a34
Yazan Al-Fanek <99050281+Alpha-nek@users.noreply.github.com>
2025-08-11 13:18:38 -0400

    Update app.py

--- app.py
+++ app.py
@@ -538,7 +538,7 @@ def top_controls():
         st.session_state.highlight_provider = "" if sel == "(All providers)" else sel
 
     # Generate & Validate buttons
-    g1, g2, g3 = st.columns(4)
+    g1, g2, g3 = st.columns(3)
     with g1:
         if st.button("Generate Draft from Rules"):
             providers = st.session_state.providers_df["initials"].tolist()
@@ -937,6 +937,7 @@ def main():
 
 if __name__ == "__main__":
     main()
+
 
 
 
